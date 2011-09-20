08e77804823be82b316624b6ed0eb93c03890601
marioosh <marioosh.net@gmail.com>
2011-09-20 11:53:42 +0200

    junit tests, zaczynamy...

---
+++ src/test/java/net/marioosh/springstarter/test/FirstTest.java
@@ -0,0 +1,22 @@
+package net.marioosh.springstarter.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class FirstTest {
+
+	@Before
+	void setup() {
+		
+	}
+	
+	@Test
+	void test() throws Exception {
+		
+	}
+}
