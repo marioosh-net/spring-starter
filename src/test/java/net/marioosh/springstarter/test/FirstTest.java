1fcb20f0fc353278f6c8c0fbe3a2e5a89bd0feff
marioosh <marioosh.net@gmail.com>
2011-09-20 15:11:10 +0200

    @Autowired działa w testach

--- src/test/java/net/marioosh/springstarter/test/FirstTest.java
+++ src/test/java/net/marioosh/springstarter/test/FirstTest.java
@@ -1,8 +1,13 @@
 package net.marioosh.springstarter.test;
 
+import net.marioosh.springstarter.MainController;
+import net.marioosh.springstarter.model.dao.UserDAO;
+
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,13 +15,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations={"classpath:applicationContext.xml"})
 public class FirstTest {
 
+	private final Logger log = Logger.getLogger(getClass());
+	
+	@Autowired
+	private UserDAO userDAO;
+	
 	@Before
 	public void setup() {
-		
 	}
 	
 	@Test
 	public void test() throws Exception {
-		
+		log.info(userDAO);
 	}
 }
