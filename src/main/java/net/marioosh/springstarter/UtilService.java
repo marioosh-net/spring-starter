7d9d005992e108f2b099d02789c13d28914a60be
marioosh <marioosh.net@gmail.com>
2012-02-23 12:42:37 +0100

    Enable @Scheduled and @Async

--- src/main/java/net/marioosh/springstarter/UtilService.java
+++ src/main/java/net/marioosh/springstarter/UtilService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service("utilService")
@@ -15,7 +16,7 @@ public class UtilService implements Serializable, ApplicationContextAware {
 
 	private static final long serialVersionUID = 1L;
 
-	private Logger log = Logger.getLogger(getClass());
+	private Logger log = Logger.getLogger(UtilService.class);
 
 	private ApplicationContext appContext;
 
@@ -35,5 +36,12 @@ public class UtilService implements Serializable, ApplicationContextAware {
 	public String getSeparatorChar() {
 		return File.separator;
 	}
-	
+
+	/**
+	 * scheduled method 
+	 */
+	@Scheduled(fixedRate=30000)
+	public void every30Second() {
+		log.info("every30Second()");
+	}
 }
